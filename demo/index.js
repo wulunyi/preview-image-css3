e3f5c4803422a66fd26a8a024fb0cff5f883cc2c
吴伦毅 <wulunyi@weidian.com>
2017-03-23 15:40:19 +0800

    rename

--- demo/index.js
+++ demo/index.js
@@ -1,12 +1,12 @@
 import './index.scss';
-import PreviewImage from '../src/preview-image-css3';
+import PreviewImage from '../lib/preview-image-css3';
 import Gesture from 'alloyfinger';
 var img = require('./cover.jpg');
 
 let rootDom = document.getElementsByClassName('app')[0];
 
 let previewImage = new PreviewImage(rootDom, {
-    imageSrc: 'https://si.geilicdn.com/bj-IM-208863409-1489732174291-2036503674-unadjust.jpg', // 图片地址
+    imageSrc: img, // 图片地址
     rotatable: false, // 是否开启旋转
     doubleZoom: 2, // 双击缩放
     maxZoom: 4, // 最大缩放
